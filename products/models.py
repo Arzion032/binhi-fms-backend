13f679179a7fb98c561f30e1905f33e8885cdbfe
Arzion032 <jmelvinsarabia032@gmail.com>
2025-05-23 17:06:48 +0800

    changes to unit_price

--- products/models.py
+++ products/models.py
@@ -59,7 +59,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', blank=True, null=True)
     vendor = models.ForeignKey(
